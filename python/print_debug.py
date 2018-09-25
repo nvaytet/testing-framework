509937b3112e283a392cb3a9fc63022281ef9b0b
Neil Vaytet <neil.vaytet@esss.se>
2018-09-25 14:19:31 +0200

    print_debug script now operational

--- python/print_debug.py
+++ python/print_debug.py
@@ -1,26 +1,64 @@
 import sys
 import shutil
 
-fname = sys.argv[1]
+in_fname = sys.argv[1]
+out_fname = in_fname+'.tmp'
 
-splitname = fname.split('.')
+splitname = in_fname.split('.')
 
-shutil.copyfile(fname, splitname[0] + "_backup." + splitname[1])
+shutil.copyfile(in_fname, splitname[0] + '_backup.' + splitname[1])
 
-fin = open(fname,'r')
-fout = open(fname+".tmp",'w')
+fin = open(in_fname,'r')
+fout = open(out_fname,'w')
+
+fout.write('#include <iostream>\n')
 
 counter = 0
-printout = "PRINT DEBUG: "
+print_open = '\n std::cout << "PRINT DEBUG: ' + in_fname
+print_close = '" << std::endl;\n'
+
+inside_one_liner_if_block = False
 
 for theLine in fin.readlines():
-	line = theLine.rstrip()
+    line = theLine.rstrip()
+    ok = []
+    if line.endswith(';'):
+        ok.append(True)
+    if line.endswith('  }'):
+        ok.append(True)
+    if line.endswith(') {'):
+        ok.append(True)
+    if line.lstrip().startswith('using'):
+        ok.append(False)
+    if line.lstrip().startswith('namespace'):
+        ok.append(False)
+    if line.lstrip().startswith('switch ('):
+        ok.append(False)
+    if line.lstrip().startswith('switch('):
+        ok.append(False)
+    if (line.lstrip().startswith('case')) and (line.endswith(':')) :
+        ok.append(True)
+    if line.endswith(' break;'):
+        ok.append(False)
+    if line.lstrip().startswith('return '):
+        ok.append(False)
 
-	if line.endswith(';') or line.endswith("  }") or line.endswith('{'):
-		fout.write(line+'\n std::cout << "'+printout+str(counter)+'" << std::endl;\n')
-		counter += 1
-	else:
-		fout.write(line+'\n')
+    # Handling if () one-liners
+    if (line.lstrip().startswith('if (')) and (line.endswith(')')):
+        counter += 1
+        fout.write(line+' {'+print_open+' '+str(counter)+print_close)
+        inside_one_liner_if_block = True
+    elif (len(ok) == 0) or (False in ok):
+        fout.write(line+'\n')
+    else:
+        counter += 1
+        fout.write(line+print_open+' '+str(counter)+print_close)
+        if inside_one_liner_if_block:
+            counter += 1
+            fout.write('}'+print_open+' '+str(counter)+print_close)
+            inside_one_liner_if_block = False
 
 fin.close()
 fout.close()
+
+shutil.move(out_fname, in_fname)
