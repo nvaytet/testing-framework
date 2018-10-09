a4fa18c9a8bc500f8ae09c499d6df953fb23eb88
Neil Vaytet <neil.vaytet@esss.se>
2018-10-09 10:29:43 +0200

    Now with absolute paths and terminal output

--- bash/clang_format_changed_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-thisBranch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
-
-for f in $(git diff --name-only master $thisBranch); do
-   clang-format-5.0 -i $f;
-done
-
---
+++ bash/clang_formatter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get git information
+thisBranch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
+topLevelDir=$(git rev-parse --show-toplevel);
+
+tempfile="clang_formatter.tmp";
+
+# Make terminal hrule
+width=$(tput cols);
+hrule="";
+for ((i=0;i<$(($width - 9));i++)); do
+    hrule="${hrule}.";
+done
+
+# Go through the list of changed files
+for f in $(git diff --name-only master $thisBranch); do
+    fname="${topLevelDir}/${f}";
+    cp $fname $tempfile;
+    clang-format-5.0 -i $fname;
+    theDiff=$(diff $fname clang_formatter.tmp);
+    theString="${f} ${hrule:${#f}}";
+    if [ ${#theDiff} -gt 0 ] ; then
+        echo "${theString} [FIXED]";
+    else
+        echo "${theString} [ OK  ]";
+    fi
+    rm $tempfile;
+done
